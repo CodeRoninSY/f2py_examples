cd2a3ab1fe6ff4060f91345e9f87fb478cc5e4d8
CodeRonin S <45330933+CodeRoninSY@users.noreply.github.com>
2019-11-30 14:50:54 +0300

    Add files via upload

--- test.f90
+++ test.f90
@@ -1,19 +1,32 @@
-subroutine test()
+subroutine test(r, x, t, n, m)
+! test.f90
+! r : real(8) (OUT) -> return value
+! x, t: real(8) (IN) -> for func args
+! m,n: integer -> dimensions of x & t
+! py_func : external f; pass from python to fortran
 
-!f2py intent(callback) py_func
-external py_func
+!f2py intent(callback) f
+external f
+! The following lines define the signature of py_func for f2py
+real(8) f
+!f2py real(8) y,arg1,arg2
+!f2py y = f(arg1, arg2)
 
-real*8 py_func
-real*8 y,x
-!f2py y = py_func(x)
+real(8), intent(in) :: x(m)
+real(8), intent(in) :: t(n)
+integer :: n, m
+!f2py depend(t) n
+!f2py depend(x) m
+real(8), INTENT(OUT) :: r(m, n)
 
-real*8 :: a
-real*8 :: b, r
+write(6,'(A4,2x,3F16.8)') "x: ", (x(i),i=1,m)
+write(6,'(A)') "i       j       t               r"
 
-a = 12
-write(6,*) a
-r = py_func(a)
-write(6,*) r
+do j = 1,m
+    do i = 1,n
+        r(j, i) = f(x(j), t(i))
+        write(6,'(I4,2x,I4,2x,F16.8,2x,E35.28)') i,j, t(i), r(j, i)
+    end do
+end do
 
-return
 end subroutine
--- test.py
+++ test.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
+"""
+test.py
+<2019-11-30> CodeRoninSY
+Pass func as argument to fortran compiled module
+$> f2py -c -m test test.f90
+$> python test.py
+"""
+from __future__ import print_function
+import math
+import numpy as np
 import test
 
 
-def func(x: float) -> float:
-    return x * 2
+if __name__ == "__main__":
+    def func(x: float, t: float) -> float:
+        return x * math.sin(t)
 
-z = 0
-z = test.test(func)
-print(z)
+    x = [1.0, 2.0, 5.0]
+    t = np.linspace(0, 2 * math.pi, 12)
+    # z = 0.0
+    z = test.test(x, t, func)
+
+    print("z: {}".format(z))
